1e44dcc845259443b63d301b4f95c9c477285348
Emre <emre_akacik_fb@hotmail.com>
2023-01-18 00:57:10 +0300

    Initial commit

---
+++ src/main/java/com/haydikodlayalim/service/KisiService.java
@@ -0,0 +1,15 @@
+package com.haydikodlayalim.service;
+
+import com.haydikodlayalim.dto.KisiDto;
+import com.haydikodlayalim.entity.Kisi;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface KisiService {
+    KisiDto save(KisiDto kisiDto );
+    void delete(Long id);
+    List<KisiDto> getAll();
+    Page<Kisi> getAll(Pageable pageable);
+}
